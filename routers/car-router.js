a5e460befe6c5970e468981444b38e57e446ab97
Clark Williams <theclarkwilliams@gmail.com>
2019-08-13 14:45:18 -0600

    get and post working, MVP

--- routers/car-router.js
+++ routers/car-router.js
@@ -6,4 +6,24 @@ const db = knex(knexConfig.development);
 
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    db('cars')
+        .then(cars => {
+            res.status(200).json(cars)
+        })
+        .catch(err => {
+            res.status(500).json({ error: "problem getting cars" })
+        })
+});
+
+router.post('/', (req, res) => {
+    const post = req.body;
+    db('cars').insert(post)
+        .then(response => {
+            res.status(201).json(response)
+        })
+        .catch(err => {
+            res.status(500).json({ error: "problem posting car" })
+        })
+})
 module.exports = router;
